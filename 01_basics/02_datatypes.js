311a5b40b8b72cbd43aa16d283383e5af0c1b533
Vijay Shinde <shindevijay595@gmail.com>
2024-11-12 05:17:51 +0530

    data types file added

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version
+console.log('Vijay Shinde')
+console.log(1+3, " Hello")
+
+// alert(3+4) // we are using node.js not browser
+
+let name = "vijay"
+let age = 29
+let isLoggedIn = true
+let state = null
+let state_n 
+
+console.table([name, age, isLoggedIn, state, state_n])
+console.log(typeof name)
+
+/*
+int = 2 to power 53
+bigint
+string = "vijay"
+boolean = true.false
+null = standalone value
+undefined
+symbol = unique
+
+object
+*/
