094014475acc28a434d9c7996385847097578296
Vijay Shinde <shindevijay595@gmail.com>
2024-11-12 05:44:32 +0530

    new topic added

---
+++ 01_basics/StringToNumber.js
@@ -0,0 +1,20 @@
+console.log("Vijay SHinde");
+
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+let a,b,c = 5
+console.log(c);
+
+let count = 12
+count++ //postfix - 
+++count // prefix - 
+console.log(count);
+
---
+++ 01_basics/datatypeconversions.js
@@ -0,0 +1,30 @@
+let score = "33"
+let valueScore = Number(score)
+
+let a = 5
+let Newa = String(a)
+
+
+console.log(score);
+console.log(typeof(score));
+console.log(typeof(valueScore));
+console.log(valueScore);
+
+console.log(typeof(Newa));
+console.log("this is new value:" + Newa);
+let x = 5
+x = 3
+
+console.log(x);
+
+let y = 0
+let newy = Boolean(y)
+console.log(y);
+console.log(newy)
+
+
+
+/*
+
+*/
+
