dade05ee7d383dae4eda4c6daa3e39234dccd36d
Vijay Shinde <shindevijay595@gmail.com>
2024-11-14 05:59:41 +0530

    New Modules added

---
+++ 02_basics/objects2.js
@@ -0,0 +1,33 @@
+//console.log("Obbjects");
+
+//let tinderUser = new Object()
+// const tinderUser = {}
+
+// tinderUser.id = "1"
+// tinderUser.name = "samay"
+// tinderUser.isLoggedIn = false
+// console.log(tinderUser);
+
+// const regularUser = {email:"example@gmail.com", fullName: {firstname:"vijay", lastname:"shinde"}}
+// console.log(regularUser);
+// console.log(regularUser["email"])
+
+// regularUser["email"] = "example2@gmail.com"
+// console.log(regularUser);
+
+// const obj1 = {1:"a", 2:"b", 3:"c"}
+const obj2 = {"d":4, "e":5, "f":6}
+// //const obj3 = Object.assign(obj1, obj2) // (target, value)
+// const obj4 = Object.assign({}, obj1, obj2)
+
+// //console.log(obj3);
+// console.log(obj4);
+// console.log(obj1);
+console.log(Object.entries(obj2));
+console.log(Object.values(obj2));
+
+
+
+
+
+
---
+++ 02_basics/objects3.js
@@ -0,0 +1,17 @@
+// Object destructuring 
+
+// const course = {coursename:"JS Hindi", price:"999", courseinstructor:"hitest"}
+
+// console.log(course["coursename"]);
+
+// const {courseinstructor} = course 
+// console.log(courseinstructor);
+
+// const {courseinstructor:instructor} = course 
+// console.log(instructor);
+
+// API
+
+// {"name":"hitest",    "course":"JS",    "price":"free", "age":25} // JSON
+// [{"name":"hitest",    "course":"JS",    "price":"free", "age":25}] // Array
+//{"results":[{"gender":"male","name":{"title":"Mr","first":"Emre","last":"Alyanak"},"location":{"street":{"number":7837,"name":"Filistin Cd"},"city":"Hatay","state":"Yalova","country":"Turkey","postcode":85143,"coordinates":{"latitude":"69.5305","longitude":"-23.9796"},"timezone":{"offset":"+2:00","description":"Kaliningrad, South Africa"}},"email":"emre.alyanak@example.com","login":{"uuid":"518c96fd-1d69-4116-a0d8-1f62114ca480","username":"ticklishlion188","password":"666666","salt":"f3AMfhxl","md5":"ec93abf9de89dafcfc5b89d724078e31","sha1":"8cfc5c0fe0fbb6fc90884e9c0d453a9cafabd814","sha256":"669bc806d45e6653b18fdd5cbfb6569484a91fb54519cc9e04e394d2f6af2e2d"},"dob":{"date":"1997-11-13T09:10:20.253Z","age":27},"registered":{"date":"2009-10-14T15:49:21.383Z","age":15},"phone":"(002)-763-5079","cell":"(064)-173-7803","id":{"name":"","value":null},"picture":{"large":"https://randomuser.me/api/portraits/men/12.jpg","medium":"https://randomuser.me/api/portraits/med/men/12.jpg","thumbnail":"https://randomuser.me/api/portraits/thumb/men/12.jpg"},"nat":"TR"}],"info":{"seed":"d9836b816cf70392","results":1,"page":1,"version":"1.4"}}
